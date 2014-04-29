25ed2ff9742f0aaa73657a793cf7e9a15431f838
Nick Prestel <nick.prestel@gmail.com>
2014-04-29 19:52:20 -0400

    added CanCan error page redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to shipments_url,
+    error: "Access denied."
+  end
+  
 end
